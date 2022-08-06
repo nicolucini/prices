96bc9f56486dbe128128c92996928d99d2010b66
Nicolas Lucini <nicolas.lucini@etermax.com>
2022-08-05 22:19:55 -0300

    Setup project

---
+++ src/test/java/com/test/prices/PricesApplicationTests.java
@@ -0,0 +1,12 @@
+package com.test.prices;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PricesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
